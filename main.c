2a355a8c207add8d60e6c9b378bc502d4109bb0f
Humanogeovs <marcogeovane71@gmail.com>
2025-03-26 15:59:08 -0300

    ajuste de codificação para suporte a caracteres especiais em português

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <locale.h>
  
 
  
 /////
  
-// Assinatura das fun√ß√µes
+// Assinatura das funÁıes
  
 void tela_principal(void);
  
@@ -23,7 +24,7 @@ void tela_cadastrar_produto(void);
 // Programa principal
  
 int main(void) {
- 
+    setlocale(LC_ALL, "Portuguese_Brazil");
     tela_principal();
         getchar();
     tela_sobre();
@@ -44,7 +45,7 @@ int main(void) {
  
 /////
  
-// Fun√ß√µes
+// FunÁıes
  
 
  
@@ -53,21 +54,21 @@ void tela_principal(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///               Departamento de Computa√ß√£o e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programa√ß√£o                      ///\n");
-    printf("///                          Projeto Sig-Chap√©u                             ///\n");
+    printf("///               Departamento de ComputaÁ„o e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- ProgramaÁ„o                      ///\n");
+    printf("///                          Projeto Sig-ChapÈu                             ///\n");
     printf("///             Developed by @name.is.marco -- since Mar, 2023              ///\n");
-    printf("///               Projeto Sistema de Gest√£o de Chapelaria                   ///\n");
+    printf("///               Projeto Sistema de Gest„o de Chapelaria                   ///\n");
     printf("///                  Developed by @HumanoGeovs- 2025.1                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
-    printf("///            1. M√≥dulo Cliente                                            ///\n");
-    printf("///            2. M√≥dulo Administrador                                      ///\n");
-    printf("///            3. M√≥dulo Equipe                                             ///\n");
-    printf("///            4. M√≥dulo Encomendas                                         ///\n");
-    printf("///            5. M√≥dulo Relat√≥rios                                         ///\n");
+    printf("///            1. MÛdulo Cliente                                            ///\n");
+    printf("///            2. MÛdulo Administrador                                      ///\n");
+    printf("///            3. MÛdulo Equipe                                             ///\n");
+    printf("///            4. MÛdulo Encomendas                                         ///\n");
+    printf("///            5. MÛdulo RelatÛrios                                         ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///                                                                         ///\n");
@@ -87,19 +88,19 @@ void tela_sobre(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///               Departamento de Computa√ß√£o e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programa√ß√£o                      ///\n");
-    printf("///                          Projeto Sig-Chap√©u                             ///\n");
+    printf("///               Departamento de ComputaÁ„o e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- ProgramaÁ„o                      ///\n");
+    printf("///                          Projeto Sig-ChapÈu                             ///\n");
     printf("///             Developed by @name.is.marco -- since Mar, 2023              ///\n");
-    printf("///               Projeto Sistema de Gest√£o de Chapelaria                   ///\n");
+    printf("///               Projeto Sistema de Gest„o de Chapelaria                   ///\n");
     printf("///                  Developed by @HumanoGeovs- 2025.1                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///         = = = = = Sistema de Gest√£o de Chapelaria = = = = =             ///\n");
+    printf("///         = = = = = Sistema de Gest„o de Chapelaria = = = = =             ///\n");
     printf("///                                                                         ///\n");
-    printf("///  Programa visa facilitar encomendas e previs√£o de entregas  de uma     ///\n");
-    printf("///  Chapelaria ou estabelecimentos semelhantes que trabalhem com este ou  ///\n");
+    printf("///  Programa visa facilitar encomendas e previs„o de entregas  de uma      ///\n");
+    printf("///  Chapelaria ou estabelecimentos semelhantes que trabalhem com este ou   ///\n");
     printf("///  outro tipo de produto, para assim facilitar tanto para o cliente quanto///\n");
     printf("///  para os funcionarios da chapelaria ou semelhante.                      ///\n");
     printf("///                                                                         ///\n");
@@ -118,17 +119,17 @@ void tela_equipe(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///               Departamento de Computa√ß√£o e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programa√ß√£o                      ///\n");
-    printf("///                          Projeto Sig-Chap√©u                             ///\n");
+    printf("///               Departamento de ComputaÁ„o e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- ProgramaÁ„o                      ///\n");
+    printf("///                          Projeto Sig-ChapÈu                             ///\n");
     printf("///             Developed by @name.is.marco -- since Mar, 2023              ///\n");
-    printf("///               Projeto Sistema de Gest√£o de Chapelaria                   ///\n");
+    printf("///               Projeto Sistema de Gest„o de Chapelaria                   ///\n");
     printf("///                  Developed by @HumanoGeovs- 2025.1                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");;
     printf("///            Este projeto foi desenvolvido por:                           ///\n");
     printf("///                                                                         ///\n");
-    printf("///            Marco Geovane Ara√∫jo                                         ///\n");
+    printf("///            Marco Geovane Ara˙jo                                         ///\n");
     printf("///            E-mail: marcogeovane71@gmail.com                             ///\n");
     printf("///            Redes sociais: @Name.Is.Marco                                ///\n");
     printf("///            Git: https://github.com/HumanoGeovs/Sig_chapeu2.git          ///\n");
@@ -144,11 +145,11 @@ void tela_produtos(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///               Departamento de Computa√ß√£o e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programa√ß√£o                      ///\n");
-    printf("///                          Projeto Sig-Chap√©u                             ///\n");
+    printf("///               Departamento de ComputaÁ„o e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- ProgramaÁ„o                      ///\n");
+    printf("///                          Projeto Sig-ChapÈu                             ///\n");
     printf("///             Developed by @name.is.marco -- since Mar, 2023              ///\n");
-    printf("///               Projeto Sistema de Gest√£o de Chapelaria                   ///\n");
+    printf("///               Projeto Sistema de Gest„o de Chapelaria                   ///\n");
     printf("///                  Developed by @HumanoGeovs- 2025.1                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -173,11 +174,11 @@ void tela_cadastrar_produto(void) {
     printf("\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///               Departamento de Computa√ß√£o e Tecnologia                   ///\n");
-    printf("///                  Disciplina DCT1106 -- Programa√ß√£o                      ///\n");
-    printf("///                          Projeto Sig-Chap√©u                             ///\n");
+    printf("///               Departamento de ComputaÁ„o e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- ProgramaÁ„o                      ///\n");
+    printf("///                          Projeto Sig-ChapÈu                             ///\n");
     printf("///             Developed by @name.is.marco -- since Mar, 2023              ///\n");
-    printf("///               Projeto Sistema de Gest√£o de Chapelaria                   ///\n");
+    printf("///               Projeto Sistema de Gest„o de Chapelaria                   ///\n");
     printf("///                  Developed by @HumanoGeovs- 2025.1                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
@@ -185,8 +186,8 @@ void tela_cadastrar_produto(void) {
     printf("///                 = = = = = Cadastrar Produto = = = = =                   ///\n");
     printf("///                                                                         ///\n");
     printf("///            Nome do Produto:                                             ///\n");
-    printf("///            C√≥digo do Produto:                                           ///\n");
-    printf("///            Descri√ß√£o do Produto:                                        ///\n");
+    printf("///            CÛdigo do Produto:                                           ///\n");
+    printf("///            DescriÁ„o do Produto:                                        ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("-----------      pressione ENTER para ir para a proxima tela     --------------\n");
